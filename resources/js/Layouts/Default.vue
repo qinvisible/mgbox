9e8d1443facb801482822d7baaee87e3e37593ab
Adi Utama <neogatotkaca@gmail.com>
2024-01-09 11:22:18 +0700

    Refactor Default.vue layout to include a sidebar with navigation links

--- resources/js/Layouts/Default.vue
+++ resources/js/Layouts/Default.vue
@@ -1,7 +1,23 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
 <template>
-    <div class="relative h-screen overflow-hidden">
-        <div class="w-full p-6">
-            <slot />
+    <div class="relative flex flex-row h-screen overflow-hidden">
+        <div class="drawer lg:drawer-open">
+            <input id="app-drawer" type="checkbox" class="drawer-toggle" />
+
+            <div class="drawer-content p-4">
+                <slot />
+            </div>
+
+            <div class="drawer-side">
+                <label for="app-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+                <ul class="menu p-4 w-48 min-h-full bg-base-200 text-base-content">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/role">Role</Link></li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
