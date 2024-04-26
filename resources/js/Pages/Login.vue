b2895db78fe15ef8d5662db256fd75018dd0fbd3
Adi Utama <neogatotkaca@gmail.com>
2024-04-26 23:52:31 +0700

    Update Login page layout to use Island layout

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,37 +1,40 @@
 <script>
-import SimpleLayout from '@/Layouts/Simple.vue'
+import IslandLayout from '@/Layouts/Island.vue'
 
 export default {
-    layout: SimpleLayout,
+    layout: IslandLayout,
 }
 </script>
 
 <template>
-    <div>
-        <h1 class="text-3xl font-semibold text-center">Login</h1>
-
-        <form class="space-y-3">
-            <div>
-                <label class="label">
-                    <span class="text-base label-text">Email</span>
-                </label>
-                <input type="text" name="email" placeholder="Enter Email" class="input input-bordered w-full">
-            </div>
-
-            <div>
-                <label class="label">
-                    <span class="text-base label-text">Password</span>
-                </label>
-                <input type="password" name="password" placeholder="Enter Password" class="input input-bordered w-full">
-            </div>
-
-            <a href="#" class="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
-
-            <div>
-                <button type="submit" class="btn btn-primary w-full">
-                    Login
-                </button>
-            </div>
-        </form>
+    <div class="flex w-full lg:w-1/2 m-10 lg:m-0 overflow-hidden rounded-xl bg-white">
+        <div class="flex-1 hidden lg:block">
+            <img src="@assets/img/login-image.jpg" alt="Login" class="w-full h-full object-cover">
+        </div>
+        <div class="flex-1 p-10 space-y-10">
+            <h1 class="text-2xl font-semibold">Login</h1>
+            <form class="space-y-3">
+                <div>
+                    <label class="label">
+                        <span class="text-base label-text">Email</span>
+                    </label>
+                    <input type="text" name="email" placeholder="Enter Email" class="input input-bordered w-full">
+                </div>
+                <div>
+                    <label class="label">
+                        <span class="text-base label-text">Password</span>
+                    </label>
+                    <input type="password" name="password" placeholder="Enter Password" class="input input-bordered w-full">
+                </div>
+                <div class="text-right">
+                    <a href="#" class="text-sm text-natural-600 font-semibold hover:underline hover:text-blue-600">Forget Password?</a>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary w-full">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
