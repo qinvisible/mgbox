b52cd4eb679ddcd5200c13134cb8491f56de3539
Adi Utama <neogatotkaca@gmail.com>
2024-04-26 23:52:18 +0700

    Rename Simple layout to Island

---
+++ resources/js/Layouts/Island.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="relative flex flex-col h-screen overflow-hidden justify-center bg-neutral-100">
+        <div class="flex justify-center">
+            <slot />
+        </div>
+    </div>
+</template>
--- resources/js/Layouts/Simple.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="relative flex flex-col h-screen overflow-hidden items-center justify-center">
-        <div class="w-full p-6 lg:max-w-lg">
-            <slot />
-        </div>
-    </div>
-</template>
