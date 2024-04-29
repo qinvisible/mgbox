ec5c1bf014ded4f75a011620e57fdd5122c49b45
Adi Utama <neogatotkaca@gmail.com>
2024-04-29 09:21:37 +0000

    Add Navbar component

---
+++ resources/js/Components/Navbar/Item.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    level: {
+        type: Number,
+        default: 3,
+    },
+});
+</script>
+
+<template>
+    <div v-if="level > 0">
+        <span>Iseng aja</span>
+        <Item :level="level - 1" />
+    </div>
+</template>
---
+++ resources/js/Components/Navbar/Navbar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <nav class="navbar px-8 text-white bg-neutral-900">
+        <div class="flex-1">
+            <Link href="/" class="text-xl">{{ props.title }}</Link>
+        </div>
+
+        <div class="flex-none">
+            <div className="avatar placeholder">
+                <div className="bg-neutral text-neutral-content rounded-full w-8">
+                    <span className="text-xs">MG</span>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
