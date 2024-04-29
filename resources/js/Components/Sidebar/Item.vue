6938a1a93f3cf4156c8725dd9bc99ad2f60f8d53
Adi Utama <neogatotkaca@gmail.com>
2024-04-29 09:21:51 +0000

    Add Sidebar component

---
+++ resources/js/Components/Sidebar/Item.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { Icon } from '@iconify/vue';
+
+const props = defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+    },
+    href: {
+        type: String,
+    },
+    items: {
+        type: Array,
+        default: () => [],
+    },
+});
+</script>
+
+<template>
+    <template v-if="props.items.length">
+        <details open>
+            <summary>
+                <Icon v-if="props.icon" :icon="props.icon" /> {{ props.label }}
+            </summary>
+            <ul>
+                <li v-for="item in props.items">
+                    <Item v-bind="item" />
+                </li>
+            </ul>
+        </details>
+    </template>
+    <template v-else>
+        <Link :href="props.href">
+            <Icon v-if="props.icon" :icon="props.icon" /> {{ props.label }}
+        </Link>
+    </template>
+</template>
---
+++ resources/js/Components/Sidebar/Sidebar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Item from './Item.vue';
+
+const props = defineProps({
+    items: Array,
+});
+</script>
+
+<template>
+    <aside class="min-h-full">
+        <ul class="menu h-full bg-neutral text-neutral-content">
+            <li v-for="item in props.items">
+                <Item v-bind="item"/>
+            </li>
+        </ul>
+    </aside>
+</template>
